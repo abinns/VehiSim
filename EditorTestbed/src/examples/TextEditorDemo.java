6d1db8e04c202f19ad63850fae3f1cf64bbfffce
Andrew Binns <abinns2011@my.fit.edu>
2016-05-17 19:33:14 -0400

    Fixed for sanity

--- EditorTestbed/src/examples/TextEditorDemo.java
+++ EditorTestbed/src/examples/TextEditorDemo.java
@@ -36,13 +36,8 @@ public class TextEditorDemo extends JFrame
 	public static void main(String[] args)
 	{
 		// Start all Swing applications on the EDT.
-		SwingUtilities.invokeLater(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				new TextEditorDemo().setVisible(true);
-			}
+		SwingUtilities.invokeLater(() -> {
+			new TextEditorDemo().setVisible(true);
 		});
 	}
 
